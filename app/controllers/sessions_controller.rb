9ae9fa7cc1922d4072278857e72deba25af7838e
zahidalik <zahidkhan545@icloud.com>
2020-04-10 20:32:55 +0300

    Create login for tutors and students

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class SessionsController < ApplicationController
+  def new_login_tutor
+  end
+
+  def new_login_student
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#welcome'
+  get 'login-tutor', to: 'sessions#new_login_tutor'
+  get 'login-student', to: 'sessions#new_login_student'
 end

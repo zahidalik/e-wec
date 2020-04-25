e87477cdd5306d2961b1a5748cca6cc7971c8876
zahidalik <zahidkhan545@icloud.com>
2020-04-25 10:28:37 +0300

    Add audio attachment and refactor

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -48,6 +48,6 @@ class LessonsController < ApplicationController
   end
 
   def lesson_params
-    params.require(:lesson).permit(:subject, :topic, :description, :body, :video)
+    params.require(:lesson).permit(:subject, :topic, :description, :body, :video, :audio)
   end
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,7 @@ class Lesson < ApplicationRecord
   belongs_to :standard
   has_rich_text :body
   has_one_attached :video
+  has_one_attached :audio
 
   validates :subject, :topic, :body, presence:true
   default_scope { order(created_at: :desc) }

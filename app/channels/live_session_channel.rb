e8d293fd53d8a69a554c2a5a353c3e224d0b3679
zahidalik <zahidkhan545@icloud.com>
2020-06-13 12:10:36 +0300

    Destroy LiveSession channel and go for production

--- app/channels/live_session_channel.rb
+++
@@ -1,9 +0,0 @@
-class LiveSessionChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "live_session_channel"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
